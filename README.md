8a48eb72689c17377ee6516e1c6f230a2123c03c
LEEDUCKHEE <shldhee@yahoo.co.kr>
2019-04-29 02:17:41 +0900

    added js

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # study-project
 개인 스터디 및 연습 정리
+
+## id
+
+- 커뮤니티 joycommuntiy
+- 컨쉽워페어 joygw
+- 통합포털 joyportal
+- 나를 지켜줘 joysavior
+- 3on3 joy3on3
+- 창세기전 joygnss
+- 나사렛대학교 k2kornu
+- 강원도 교육청 k2gwe
+- 연안대학교 k2yonam
+- 인천장애인콜택시 k2intis
+- 고려대학교 k2korea
+
+## TODO
+
+- navigation 이동
+- infomation 작성
+- vue study 정리
+- study 정리
+- responsive 대응
---
+++ js/main.js
@@ -0,0 +1,101 @@
+import { projectObject } from '/js/projectObject.js';
+
+// 변수 선언
+const popup = document.querySelector('.popup');
+const projectLink = document.querySelectorAll('.project__link');
+const project = document.querySelector('.project');
+const toyProject = document.querySelector('.project-toy');
+const studyProject = document.querySelector('.project-study');
+
+// project 리스트 필터
+const projectList = projectObject.filter(projectObj => {
+  return projectObj.category === "PROJECT";
+});
+// toy project 리스트 필터
+const toyProjectList = projectObject.filter(projectObj => {
+  return projectObj.category === "TOY PROJECT";
+});
+// study project 리스트 필터
+const studyProjectList = projectObject.filter(projectObj => {
+  return projectObj.category === "STUDY";
+});
+
+// event
+document.addEventListener('click', function (e) {
+  // 팝업 닫기 이벤트
+  if (e.target.className === "popup__close") {
+    e.preventDefault();
+    popup.classList.remove('on');
+  }
+
+  // 프로젝트 상세 설명 클릭했을때
+  if (~e.target.className.indexOf("project__link")) {
+    // id가 같은 프로젝트 선택
+    const filteredProject = projectObject.filter(project => project.id === e.target.dataset.id)[0];
+    const projectDescription = filteredProject.description.reduce((render, description) => {
+      return render += `<li class="popup__group--item">- ${description}</li>`
+    }, "")
+
+    popup.innerHTML = `
+    <div class="popup__content">
+      <div class="popup__title">
+        ${filteredProject.subTitle}
+      </div>
+      <div class="popup__group">
+        <h4 class="popup__group--heading">
+          Technology
+        </h4>
+        <p class="popup__group--list popup_technology">
+          ${filteredProject.technology}
+        </p>
+      </div>
+      <div class="popup__group">
+        <h4 class="popup__group--heading">
+          Concept
+        </h4>
+        <p class="popup__group--list popup_concept">
+          ${filteredProject.concept}
+        </p>
+      </div>
+      <div class="popup__group">
+        <h4 class="popup__group--heading">
+          Description
+        </h4>
+        <ul class="popup__group--list popup_description">
+          ${projectDescription}
+        </ul>
+      </div>
+      <a href="#" class="popup__close">&times;</a>
+      <a href="${filteredProject.link}" target="_blank" class="popup__btn btn btn--animated btn--grey btn--rect">사이트 바로 가기 &gt;</a>
+    </div>`
+    popup.classList.add('on');
+  }
+})
+
+// function 정의
+// 프로젝트 리스트 렌더링 함수
+const renderList = function(projectObject, renderProject) {
+  const projectList = projectObject.map(projectObj => {
+    return `<div class="project__item">
+            <div class="project__card project__card-front">
+              <div class="project__box">
+                <!-- <i class="project__icon fa fa-github" aria-hidden="true"></i> -->
+                <h3 class="heading-tertiary">${projectObj.title}</h3>
+                <p class="project__paragraph">${projectObj.category}(${projectObj.type})</p>
+              </div>
+            </div>
+            <div class="project__card project__card-back">
+              <a data-id="${projectObj.id}" class="project__link btn btn--grey btn--animated" href="#;">상세 설명</a>
+            </div>
+          </div>`
+  }).join("");
+  renderProject.innerHTML = projectList;
+}
+
+// function 실행
+// 프로젝트 리스트 그리기
+renderList(projectList, project);
+renderList(toyProjectList, toyProject);
+renderList(studyProjectList, studyProject);
+
+
